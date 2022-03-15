07279a63cc0bb6a180cbc59bb3e67bccfb685992
SYEDAHAMED29 <syedahamed483@gmail.com>
2022-03-15 18:28:44 +0530

    First question

---
+++ DisplayName.java
@@ -0,0 +1,21 @@
+
+
+/* 1. Write Text‐Based Application using Object‐Oriented Approach to display your name. */
+
+public class DisplayName {
+	
+	public void display() {
+		System.out.println("Syed Aashiq Ahamed");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		DisplayName name = new DisplayName();
+		
+		name.display();
+		
+
+	}
+
+}

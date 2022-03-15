c8e13264e8601b5e12950f71d003a9b68963727d
SYEDAHAMED29 <syedahamed483@gmail.com>
2022-03-15 18:34:11 +0530

    Fourth question

---
+++ DisplayNumbers.java
@@ -0,0 +1,28 @@
+
+
+// 4 . Write an application that displays the numbers 1 to 4 on the same line, with each pair of 
+//adjacent numbers separated by one space. Write the application using the following 
+//techniques: 
+//a. Use one System.out.println statement. 
+//b. Use four System.out.print statements. 
+//c. Use one System. out. printf statement.
+
+public class DisplayNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("1 2 3 4");
+
+       
+        System.out.print("1 ");
+        System.out.print("2 ");
+        System.out.print("3 ");
+        System.out.print("4 \n");
+
+    
+        System.out.printf("%d %d %d %d \n", 1, 2, 3, 4);
+
+	}
+
+}
